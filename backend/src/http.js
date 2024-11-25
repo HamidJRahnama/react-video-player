f81be11546314f3b277a3bb126ecc4857b793c24
Hamid.J.Rahnama <hamidrahnamapc@gmail.com>
2024-11-25 20:24:32 +0330

    first

--- backend/src/http.js
+++ backend/src/http.js
@@ -1,18 +1,18 @@
 const { createServer } = require("http");
 const { stat, createReadStream } = require("fs");
 const { promisify } = require("util");
-const filename = "../public/videos/video1.mp4";
+
+const filename = "../public/videos/video2.mp4";
 const fileInfo = promisify(stat);
+let i = 0;
 
 createServer(async (req, res) => {
   const { size } = await fileInfo(filename);
   const range = req.headers.range;
   console.log("RANGE", range);
 
-  // res.writeHead(200, {
-  //   "Content-Length": size,
-  //   "Content-Type": "video/mp4",
-  // });
+  i++;
+  console.log(i);
 
   if (range) {
     let [start, end] = range.replace(/bytes=/, "").split("-");
@@ -20,26 +20,23 @@ createServer(async (req, res) => {
     end = end ? parseInt(end, 10) : size - 1;
 
     res.writeHead(206, {
-      "Contennt-Range": `bytes ${start}-${end}/${size}`,
+      "Content-Range": `bytes ${start}-${end}/${size}`,
       "Accept-Ranges": "bytes",
-      "Content-Length": start - end + 1,
+      "Content-Length": end - start + 1,
       "Content-Type": "video/mp4",
     });
-
-    createReadStream(filename, { start, end }).pipe(res);
+    // Create read stream for the specific range.
+    const stream = createReadStream(filename, { start, end });
+    stream.pipe(res);
   } else {
-    res.writeHead(206, {
+    // If no range header, respond with the full video file.
+    res.writeHead(200, {
       "Content-Length": size,
       "Content-Type": "video/mp4",
     });
-  }
 
-  //   res.writeHead(200, {
-  //     "Content-Length": size,
-  //     "Content-Type": "video/mp4",
-  //   });
-  // res.writeHead(200, {
-  //   "Content-type": "text",
-  // });
-  createReadStream(filename).pipe(res);
+    // Create read stream for the entire file.
+    const stream = createReadStream(filename);
+    stream.pipe(res);
+  }
 }).listen(8000, () => console.log("http://localhost:8000/"));
